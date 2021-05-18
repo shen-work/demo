420b25a2ef393da02ada995d613aebe505c5cf98
shen-work <79512300+shen-work@users.noreply.github.com>
2021-05-18 18:47:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -280,6 +280,7 @@ function Test()
     }
 
     System.MainDiv.appendChild( RowMake(menu) );
+    System.MainDiv.appendChild( ECPapi() );
     MainDivSetTimeout();
 }
 
@@ -2113,6 +2114,68 @@ function GetYtCommentThreads(VideoId,_func)
 }
 
 
+
+function ECPapi()
+{
+    var TradeNo = "shen"+System.time
+    var TradeDate = DateFormat(new Date(System.time));
+    
+    var ApiRow = {
+        "MerchantID":"2000132",
+        "MerchantTradeNo":TradeNo,//定單編號 不可重覆
+        "MerchantTradeDate":TradeDate,//2012/03/21 15:40:18
+        "PaymentType":"aio",
+        "TotalAmount":"999",//金額
+        "TradeDesc":"金流串接測試",
+        "ItemName":"商品名稱",
+        "ReturnURL":"https://shen-work.github.io/demo/ecp.html",//收到 Server 端付款結果通知後，請正確回應 1|OK。
+        "ChoosePayment":"Credit",
+        "CheckMacValue":"",//sha256加密
+        "EncryptType":"1"
+    }
+    var HashKey = "5294y06JbISpM5x9";
+    var HashIV = "v77hoKGq4kWxNNIS";
+
+
+    ApiRow.CheckMacValue = 
+    "HashKey="+HashKey+
+    "&ChoosePayment="+ApiRow.ChoosePayment+
+    "&EncryptType="+ApiRow.EncryptType+
+    "&ItemName="+ApiRow.ItemName+
+    "&MerchantID="+ApiRow.MerchantID+
+    "&MerchantTradeDate="+TradeDate+
+    "&MerchantTradeNo="+TradeNo+
+    "&PaymentType="+ApiRow.PaymentType+
+    "&ReturnURL="+ApiRow.ReturnURL+
+    "&TotalAmount="+ApiRow.TotalAmount+
+    "&TradeDesc="+ApiRow.TradeDesc+
+    "&HashIV="+HashIV;
+    
+    ApiRow.CheckMacValue = sha256(encodeURIComponent(ApiRow.CheckMacValue)).toUpperCase();
+
+
+    var form = document.createElement("form");
+    form.action = "https://payment-stage.ecpay.com.tw/Cashier/QueryTradeInfo/V5";
+    form.enctype = "application/x-www-form-urlencoded";
+    form.method = "POST";
+
+    for(var k in ApiRow)
+    {
+        form.appendChild( SpanCr(k) );
+        form.appendChild( TextCr("text",{"name":k,"value":ApiRow[k]}) );
+        form.appendChild( document.createElement("p") );
+    }
+
+    form.appendChild( TextCr("submit",{"value":"SUBMIT"}));
+
+
+    return form;
+}
+
+
+
+
+var sha256=function a(b){function c(a,b){return a>>>b|a<<32-b}for(var d,e,f=Math.pow,g=f(2,32),h="length",i="",j=[],k=8*b[h],l=a.h=a.h||[],m=a.k=a.k||[],n=m[h],o={},p=2;64>n;p++)if(!o[p]){for(d=0;313>d;d+=p)o[d]=p;l[n]=f(p,.5)*g|0,m[n++]=f(p,1/3)*g|0}for(b+="\x80";b[h]%64-56;)b+="\x00";for(d=0;d<b[h];d++){if(e=b.charCodeAt(d),e>>8)return;j[d>>2]|=e<<(3-d)%4*8}for(j[j[h]]=k/g|0,j[j[h]]=k,e=0;e<j[h];){var q=j.slice(e,e+=16),r=l;for(l=l.slice(0,8),d=0;64>d;d++){var s=q[d-15],t=q[d-2],u=l[0],v=l[4],w=l[7]+(c(v,6)^c(v,11)^c(v,25))+(v&l[5]^~v&l[6])+m[d]+(q[d]=16>d?q[d]:q[d-16]+(c(s,7)^c(s,18)^s>>>3)+q[d-7]+(c(t,17)^c(t,19)^t>>>10)|0),x=(c(u,2)^c(u,13)^c(u,22))+(u&l[1]^u&l[2]^l[1]&l[2]);l=[w+x|0].concat(l),l[4]=l[4]+w|0}for(d=0;8>d;d++)l[d]=l[d]+r[d]|0}for(d=0;8>d;d++)for(e=3;e+1;e--){var y=l[d]>>8*e&255;i+=(16>y?0:"")+y.toString(16)}return i};
 
 
 /*
